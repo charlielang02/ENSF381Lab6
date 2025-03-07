d9b93136ea031bbd14583d7a302bec12af8d1f9c
Justin <justin.vuong6@gmail.com>
2025-03-07 12:18:11 -0700

    ex3

--- jsx-exercise/src/index.js
+++ jsx-exercise/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App3 from './App3'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
