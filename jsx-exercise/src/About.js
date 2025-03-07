499358028b78f2a4d9ace43e845e54ba0149a143
Charlie Lang <charlie.lang@ucalgary.ca>
2025-03-07 12:16:53 -0700

    add About, Home, Contact components and update App to use App2

---
+++ jsx-exercise/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = ( {descr} ) => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>{descr}</p>
+        </div>
+    );
+};
+
+export default About;
